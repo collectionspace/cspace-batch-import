c5f478fc2b53778826739a065f9b7a3ca2f4c625
Mark Cooper <mark.c.cooper@outlook.com>
2020-07-19 14:53:02 -0700

    Catch all unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   # Pundit dev config:
   # after_action :verify_authorized, except: [:index]
   # after_action :verify_policy_scoped, only: :index
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = t('user.unauthorized')
+    redirect_to(request.referrer || root_path)
+  end
 end
