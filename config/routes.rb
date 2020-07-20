0205eefadd71b13b3117c17fa722ba392b20bf5b
Mark Cooper <mark.c.cooper@outlook.com>
2020-07-19 22:56:30 -0700

    Remove unused devise features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:passwords] # TODO: :registrations if configured
   root 'sites#home'
   resources :users, except: %i[create new show] do
     post :impersonate, on: :member
