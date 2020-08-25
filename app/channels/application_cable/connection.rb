13a3b9fba6201719110f260f0663dce49e2dc136
Mark Cooper <mark.c.cooper@outlook.com>
2020-08-24 17:00:26 -0700

    Fix auto-generated duplication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,12 +2,10 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    class Connection < ActionCable::Connection::Base
-      identified_by :current_user
+    identified_by :current_user
 
-      def connect
-        self.current_user = env['warden'].user || reject_unauthorized_connection
-      end
+    def connect
+      self.current_user = env['warden'].user || reject_unauthorized_connection
     end
   end
 end
