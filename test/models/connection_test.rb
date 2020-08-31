41a3ddc106452fc84e612402680553c751277ebb
Mark Cooper <mark.c.cooper@outlook.com>
2020-08-30 19:01:35 -0700

    Connection tests cont.

--- test/models/connection_test.rb
+++ test/models/connection_test.rb
@@ -1,6 +1,37 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ConnectionTest < ActiveSupport::TestCase
+  setup do
+    @params = {
+      name: 'core.dev',
+      url: 'https://core.dev.collectionspace.org/cspace-services',
+      username: 'admin@core.collectionspace.org',
+      password_ciphertext: Connection.generate_password_ciphertext('Administrator').inspect
+    }
+  end
+
+  test 'cannot create connection without a name' do
+    @params.delete(:name)
+    refute Connection.new(@params).valid?
+  end
+
+  test 'cannot create connection without a url' do
+    @params.delete(:url)
+    refute Connection.new(@params).valid?
+  end
+
+  test 'cannot create connection without a username' do
+    @params.delete(:username)
+    refute Connection.new(@params).valid?
+  end
+
+  test 'cannot create connection without a password' do
+    @params.delete(:password_ciphertext)
+    refute Connection.new(@params).valid?
+  end
+
   test 'disabling connection unsets primary (default)' do
     c1 = connections(:core_superuser)
     assert(c1.primary?)
@@ -8,7 +39,7 @@ class ConnectionTest < ActiveSupport::TestCase
     assert_not(c1.primary?)
   end
 
-  test 'set primary (default) exclusively' do
+  test 'sets primary (default) exclusively' do
     c1 = connections(:core_superuser)
     c2 = connections(:fcart_superuser)
     assert(c1.primary?)
