c69442b0c41c2fd9bdda94702241206ca2260def
Mark Cooper <mark.c.cooper@outlook.com>
2020-09-03 23:12:12 -0700

    Batch policy testing

--- test/policies/batch_policy_test.rb
+++ test/policies/batch_policy_test.rb
@@ -1,1 +1,45 @@
-# TODO
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class BatchPolicyTest < ActiveSupport::TestCase
+  test 'admin is allowed to create a batch' do
+    assert_permit BatchPolicy, users(:admin), Batch.new, :new
+  end
+
+  test 'manager is allowed to create a batch' do
+    assert_permit BatchPolicy, users(:manager), Batch.new, :new
+  end
+
+  test 'member is allowed to create a batch' do
+    assert_permit BatchPolicy, users(:minion), Batch.new, :new
+  end
+
+  test 'a user cannot create a batch if they have no connections' do
+    refute_permit BatchPolicy, users(:fishmonger), Batch.new, :new
+  end
+
+  test 'a user can create a batch' do
+    assert_permit BatchPolicy, users(:minion), Batch.new, :create
+  end
+
+  test 'admin can delete a batch' do
+    assert_permit BatchPolicy, users(:admin), batches(:superuser_default_batch), :destroy
+  end
+
+  test 'a manager can delete a batch' do
+    assert_permit BatchPolicy, users(:manager), batches(:superuser_default_batch), :destroy
+  end
+
+  test "a manager cannot delete another group's batch" do
+    refute_permit BatchPolicy, users(:manager), batches(:fishmonger_fish_batch), :destroy
+  end
+
+  test 'a member can delete their own batch' do
+    assert_permit BatchPolicy, users(:minion), batches(:minion_default_batch), :destroy
+  end
+
+  test "a member cannot delete another user's batch" do
+    assert_permit BatchPolicy, users(:manager), batches(:superuser_default_batch), :destroy
+  end
+end
