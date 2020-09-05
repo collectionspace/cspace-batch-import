6cf3d3cec90e030823d36bea774ee02352ebdf13
Mark Cooper <mark.c.cooper@outlook.com>
2020-09-05 00:03:25 -0700

    More re-org, layout additions

--- app/reflexes/application_reflex.rb
+++ app/reflexes/application_reflex.rb
@@ -4,6 +4,11 @@ class ApplicationReflex < StimulusReflex::Reflex
   # Learn more at: https://docs.stimulusreflex.com
   delegate :current_user, to: :connection
 
+  # add a slight delay to page reload
+  def reload
+    sleep 0.5
+  end
+
   def toggle_status
     model = element.dataset['model'].camelize.constantize
     id = element.dataset['id']
