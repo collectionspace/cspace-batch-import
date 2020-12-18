d3dc25b227808870d5bcca4684cd2d8c1cb6817d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2020-12-17 20:46:14 -0500

    improve reporting; then break it

--- app/jobs/process_job.rb
+++ app/jobs/process_job.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'pp'
 
 class ProcessJob < ApplicationJob
   queue_as :default
@@ -7,7 +8,7 @@ class ProcessJob < ApplicationJob
 
   def perform(process)
     manager = StepManagerService.new(
-      step: process, error_on_warning: false, save_to_file: !Rails.env.test?
+      step: process, error_on_warning: false, save_to_file: false
     )
     return if manager.cut_short?
 
@@ -16,22 +17,59 @@ class ProcessJob < ApplicationJob
     begin
       handler = process.batch.handler
       rs = RecordManagerService.new(client: process.batch.connection.client)
-      
+      mts = MissingTermService.new(batch: process.batch, save_to_file: true)
+      manager.add_file(mts.missing_term_occurrence_file, 'text/csv', :tmp)
+
+      rep = ReportService.new(name: "#{manager.filename_base}_processed.#{FILE_TYPE}",
+        columns: %i[row row_status message category],
+        save_to_file: true)
+      manager.add_file(rep.file, 'text/csv', :tmp)
+
       manager.process do |data|
-        row_data = { 'err' => [], 'warn' => [] }
-        
-        result = handler.process(data)
-        id = result.identifier
-        if id.empty?
-          row_data['err'] << 'No record identifier'
+        begin
+          result = handler.process(data)
+        rescue StandardError => e
+          manager.add_warning!
+          rep.append(process.step_num_row, 'warning', "Mapper did not return result: #{e.message}", 'mapper')
+          manager.add_message("Mapping failed for one or more records")
+          next	
+        end
+
+        unless result.terms.empty?
+          missing_terms = mts.get_missing(result.terms)
+          missing_terms.each{ |term| mts.add(term, manager.row_num) }
+          msgs = missing_terms.map{ |term| mts.message(term) }.join('; ')
+          manager.add_warning!
+          rep.append(process.step_num_row, 'warning', msgs, 'terms')
+        end
+
+
+        unless result.warnings.empty?
+          result.warnings.each{ |warning| manager.handle_processing_warning(rep, warning) }
         end
         
-        #manager.log!('ok', I18n.t('csv.ok'))
-        process.row_results[manager.row_num] = row_data
+        # id = result.identifier
+        # if id.empty?
+        #   row_data['err'] << 'No record identifier'
+        # end
+        
+        # #manager.log!('ok', I18n.t('csv.ok'))
         process.save
       end
+
+      # post-process across-batch reports
+      ## unique missing terms
+      mts.report_uniq_missing_terms
+      manager.add_file(mts.uniq_missing_terms_file, 'text/csv')
+      if mts.total_terms > 0
+        manager.add_message("Batch contains #{mts.total_terms} unique terms that do not exist in CollectionSpace")
+        manager.add_message("Batch contains #{mts.total_term_occurrences} uses of terms that do not exist in CollectionSpace")
+      end
+
+      manager.finalize_main_processing_report(rep)
       
-      manager.complete!
+      #      manager.complete!
+      manager.exception!
     rescue StandardError => e
       manager.exception!
       Rails.logger.error(e.message)
--- app/services/step_manager_service.rb
+++ app/services/step_manager_service.rb
@@ -2,11 +2,13 @@
 
 class StepManagerService
   attr_accessor :error_on_warning
-  attr_reader :file, :files, :headers, :messages, :step, :row_results
+  attr_reader :filename_base, :main_report, :files, :headers, :messages, :step, :row_results
   FILE_TYPE = 'csv'
-  HEADERS = %i[row row_status message].freeze
+  HEADERS = %i[row row_status message category].freeze
 
   def initialize(step:, error_on_warning:, save_to_file:)
+    time = Time.now
+    @filename_base = "#{step.batch.name.parameterize}-#{time.strftime('%F').delete('-')}-#{time.strftime('%R').delete(':')}"
     @files = []
     @messages = []
     @headers = HEADERS
@@ -15,10 +17,8 @@ class StepManagerService
     @save_to_file = save_to_file
     return unless @save_to_file
 
-    time = Time.now
-    filename = "#{step.prefix}_#{step.batch.name.parameterize}-#{time.strftime('%F').delete('-')}-#{time.strftime('%R').delete(':')}.#{FILE_TYPE}"
-    @file = Rails.root.join('tmp', filename)
-    @files << { file: @file, type: 'csv' }
+    @main_report = Rails.root.join('tmp', "#{@filename_base}.#{FILE_TYPE}")
+    @files << { file: @main_report, type: 'csv' }
     append(@headers)
   end
 
@@ -27,11 +27,13 @@ class StepManagerService
     step.batch.failed! unless step.batch.failed?
   end
 
-  def add_file(file, content_type)
+  # type = :tmp or :final
+  # :tmp files will not be attached to the step by attach! called in finishup!
+  def add_file(file, content_type, type = :final)
     return unless step.class::CONTENT_TYPES.include?(content_type)
     return unless File.file? file
 
-    files << { file: file, content_type: content_type }
+    files << { file: file, content_type: content_type, type: type }
   end
 
   def add_message(message)
@@ -43,11 +45,40 @@ class StepManagerService
     step.batch.failed! if error_on_warning && !step.batch.failed?
   end
 
+  def handle_processing_warning(report, warning)
+    category = warning[:category].to_s
+    report.append(step.step_num_row, 'warning', "#{warning[:field]}: #{warning[:value]}", category)
+    add_message("One or more records has warning: #{category}")
+  end
+
+  def finalize_main_processing_report(report_service)
+    errwarn = report_service.file
+    dh = data_headers
+    ah = added_headers(errwarn)
+    
+    final_report = ReportService.new(
+      name: "#{@filename_base}_full.#{FILE_TYPE}",
+      columns: dh + ah,
+      save_to_file: true
+    )
+    add_file(final_report.file, 'text/csv')
+    
+    ew = errs_and_warnings_for_merge(errwarn, ah)
+    od = orig_for_merge
+
+    od.each do |rownum, data|
+      merged = ew.key?(rownum) ? data.merge(ew[rownum]) : data
+      ah.each{ |hdr| merged[hdr] = '' unless merged.key?(hdr) }
+      final_report.append(merged)
+    end
+  end
+  
   def attach!
-    step.update(messages: messages)
+    step.update(messages: messages.uniq)
     return if files.empty?
 
     files.each do |f|
+      next if f[:type] == :tmp
       step.reports.attach(
         io: File.open(f[:file]),
         filename: File.basename(f[:file]),
@@ -89,14 +120,16 @@ class StepManagerService
   # TODO: test -- [ok, error, warning]
   # If save_to_file = true, send message for row to CSV file
   # If save_to_file = false, does nothing
-  def log!(status, message)
-    append({ row: step.step_num_row, row_status: status, message: message })
+  def log!(status, message, category = '')
+    cat = category.empty? ? category : "#{status.upcase}: #{category}"
+    append({ row: step.step_num_row, row_status: status, message: message, category: cat })
   end
 
   def finishup!
     step.update(completed_at: Time.now.utc)
     step.update_header # broadcast final status of step
     attach!
+    remove_tmp_files!
   end
 
   def kickoff!
@@ -111,7 +144,7 @@ class StepManagerService
     step.update_progress
   end
 
-  def process
+  def process(type = :initial)
     step.batch.spreadsheet.open do |csv|
       csv = CSV.open(csv.path, headers: true, encoding: 'bom|utf-8')
       loop do
@@ -120,7 +153,7 @@ class StepManagerService
         row = csv.shift
         break unless row
 
-        nudge!
+        nudge! if type == :initial
         yield row.to_hash
       rescue CSV::MalformedCSVError => e
         add_error!
@@ -146,8 +179,57 @@ class StepManagerService
   def append(message)
     return unless @save_to_file
 
-    CSV.open(file, 'a') do |csv|
+    CSV.open(main_report, 'a') do |csv|
       csv << (message.respond_to?(:key) ? message.values_at(*headers) : message.map(&:to_s))
     end
+  end
+
+  def remove_tmp_files!
+    files.each do |f|
+      next unless f[:type] == :tmp
+      File.delete(f[:file]) if File.exist?(f[:file])
+    end
+  end
+
+  def data_headers
+    row_ct = 1
+    headers = []
+    process(:subsequent) do |data|
+      break if row_ct > 1
+      headers = data.keys
+      row_ct += 1
+    end
+    headers
+  end
+
+  def added_headers(filename)
+    csv = CSV.parse(File.read(filename), headers: true)
+    csv.by_col[3].uniq
+  end
+
+  def errs_and_warnings_for_merge(filename, headers)
+    h = {}
+    CSV.foreach(filename, headers: true) do |row|
+      h[row['row']] = {} unless h.key?(row['row'])
+      headers.each do |hdr|
+        h[row['row']][hdr] = [] unless h[row['row']].key?(hdr)
+      end
+      h[row['row']][row['category']] << row['message']
+    end
+
+    h.each do |rownum, data|
+      data.transform_values!{ |val| val.join('; ') }
+    end
+    h.transform_keys(&:to_i)
+  end
+
+  def orig_for_merge
+    rowct = 2
+    h = {}
+    process(:subsequent) do |data|
+      h[rowct] = data
+      rowct += 1
+    end
+    h
   end
 end
